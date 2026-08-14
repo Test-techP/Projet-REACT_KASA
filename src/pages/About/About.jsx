15c4440e600295d265d1fa5399dc29301a1e241b
Test-techP <pauline.puga.pp@gmail.com>
2026-08-14 23:47:35 +0200

    création fichier data json pour page à propos contenu collapse
    ajout conteu collapse à propos et dl de l'icoe flèche

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,7 @@ import Banner from "../../components/Banner/Banner";
 import Collapse from "../../components/Collapse/Collapse";
 
 import aboutBanner from "../../assets/images/about-banner.jpg";
+import aboutData from "../../data/aboutData.json";
 
 import "./About.css";
 
@@ -11,25 +12,13 @@ function About() {
       <Banner image={aboutBanner} />
 
       <section className="about__collapses">
-        <Collapse
-          title="Fiabilité"
-          content="Ajoute ici le texte Fiabilité fourni dans la maquette."
-        />
-
-        <Collapse
-          title="Respect"
-          content="Ajoute ici le texte Respect fourni dans la maquette."
-        />
-
-        <Collapse
-          title="Service"
-          content="Ajoute ici le texte Service fourni dans la maquette."
-        />
-
-        <Collapse
-          title="Sécurité"
-          content="Ajoute ici le texte Sécurité fourni dans la maquette."
-        />
+        {aboutData.map((item) => (
+          <Collapse
+            key={item.id}
+            title={item.title}
+            content={item.content}
+          />
+        ))}
       </section>
     </div>
   );
